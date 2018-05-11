e71999b04ce61b22f4e5d3d6d2c4df1f4794d397
Zhuyunfeng <i@zhuyunfeng.com>
2018-05-11 17:34:24 +0800

    update lifecycle

--- practice/lifecycle/index.js
+++ practice/lifecycle/index.js
@@ -2,21 +2,21 @@ import Vue from 'vue'
 
 const app = new Vue({
   // el: '#root',
-  template: '<div>{{text}}</div>',
+  // template: '<div>{{text}}</div>',
   data: {
     text: 0
   },
   beforeCreate () {
-    console.log(this, 'beforeCreate')
+    console.log(this.$el, 'beforeCreate')
   },
   created () {
-    console.log(this, 'created')
+    console.log(this.$el, 'created')
   },
   beforeMount () {
-    console.log(this, 'beforeMount')
+    console.log(this.$el, 'beforeMount')
   },
   mounted () {
-    console.log(this, 'mounted')
+    console.log(this.$el, 'mounted')
   },
   beforeUpdate () {
     console.log(this, 'beforeUpdate')
@@ -35,6 +35,17 @@ const app = new Vue({
   },
   destroyed () {
     console.log(this, 'destroyed')
+  },
+  render (h) {
+    // throw new TypeError('render error')
+    console.log('render function invoked')
+    return h('div', {}, this.text)
+  },
+  renderError (h, err) {
+    return h('div', {}, err.stack)
+  },
+  errorCaptured () {
+    // 会向上冒泡，正式环境可以使用
   }
 })
 
