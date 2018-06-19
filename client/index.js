399b879e61b374870eb44b77cd3edfebf3302b0e
unknown <i@zhuyunfeng.com>
2018-06-19 18:24:22 +0800

    add vue-router

--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './app.vue'
+
+import createRouter from './config/router'
 
 import './assets/styles/global.styl'
 
@@ -7,9 +10,10 @@ import './assets/styles/global.styl'
 // import './assets/styles/test-stylus.styl'
 import './assets/images/bg.jpg'
 
-const root = document.createElement('div')
-document.body.appendChild(root)
+Vue.use(VueRouter)
+const router = createRouter()
 
 new Vue({
+  router,
   render: (h) => h(App)
-}).$mount(root)
+}).$mount('#root')
